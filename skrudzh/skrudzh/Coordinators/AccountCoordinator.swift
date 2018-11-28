d34613ece6f85ff840e1b43fbf106f846da42a54
Alex Petropavlovsky <petalvlad@gmail.com>
2018-11-28 19:51:12 +0700

    Change AccountCoordinator

---
+++ skrudzh/skrudzh/Coordinators/AccountCoordinator.swift
@@ -0,0 +1,94 @@
+//
+//  AccountCoordinator.swift
+//  skrudzh
+//
+//  Created by Alexander Petropavlovsky on 28/11/2018.
+//  Copyright © 2018 rubikon. All rights reserved.
+//
+
+import Foundation
+import PromiseKit
+
+enum AuthProviderError : Error {
+    case emailHasAlreadyUsed
+    case userIdIsNotSpecified
+    case authenticationIsCancelled
+    case canNotGetProviderSessionData
+    case canNotGetProviderUserData
+}
+
+class AccountCoordinator : AccountCoordinatorProtocol {
+    private let userSessionManager: UserSessionManagerProtocol
+    private let authenticationService: AuthenticationServiceProtocol
+    private let usersService: UsersServiceProtocol
+    private let router: ApplicationRouterProtocol
+    private let notificationsCoordinator: NotificationsCoordinatorProtocol
+    
+    init(userSessionManager: UserSessionManagerProtocol,
+         authenticationService: AuthenticationServiceProtocol,
+         usersService: UsersServiceProtocol,
+         router: ApplicationRouterProtocol,
+         notificationsCoordinator: NotificationsCoordinatorProtocol) {
+        
+        self.userSessionManager = userSessionManager
+        self.authenticationService = authenticationService
+        self.usersService = usersService
+        self.router = router
+        self.notificationsCoordinator = notificationsCoordinator
+    }
+    
+    func authenticate(email: String, password: String) -> Promise<Session> {
+        let authenticationPromise = authenticationService.authenticate(email: email, password: password)
+        _ = authenticationPromise.then { session -> Void in
+            self.userSessionManager.save(session: session)
+            self.router.route()
+        }
+        return authenticationPromise
+    }
+    
+    func createAndAuthenticateUser(with userForm: UserCreationForm) -> Promise<Session> {
+        return usersService.createUser(with: userForm).then { user in
+            return self.authenticate(email: userForm.email!, password: userForm.password!)
+        }
+    }
+    
+    func update(user: User) -> Promise<Void> {
+        return usersService.update(user: user)
+    }
+    
+    func changePassword(with changePasswordForm: ChangePasswordForm) -> Promise<Void> {
+        changePasswordForm.userId = userSessionManager.currentSession?.userId
+        return usersService.changePassword(with: changePasswordForm)
+    }
+    
+    func resetPassword(with resetPasswordForm: ResetPasswordForm) -> Promise<Void> {
+        guard let email = resetPasswordForm.email, let password = resetPasswordForm.newPassword else {
+            return Promise(error: ResetPasswordFormError.invalidCredentials)
+        }
+        return usersService.resetPassword(with: resetPasswordForm).then { _ -> Promise<Void> in
+            return self.authenticate(email: email, password: password).asVoid()
+        }
+    }
+    
+    func createConfirmationCode(email: String) -> Promise<Void> {
+        return usersService.createConfirmationCode(email: email)
+    }
+    
+    func loadCurrentUser() -> Promise<User> {
+        guard let currentUserId = userSessionManager.currentSession?.userId else {
+            return Promise(error: SessionError.noSessionInAuthorizedContext)
+        }
+        return usersService.loadUser(with: currentUserId)
+    }
+    
+    func logout() -> Promise<Void> {
+        let previousSession = userSessionManager.currentSession
+        userSessionManager.forgetSession()
+        notificationsCoordinator.cancelAllSceduledNotifications()
+        router.route()
+        guard let session = previousSession else {
+            return Promise(value: ())
+        }
+        return self.authenticationService.destroy(session: session)
+    }
+}
---
+++ skrudzh/skrudzh/Coordinators/Protocols/AccountCoordinatorProtocol.swift
@@ -0,0 +1,22 @@
+//
+//  AccountCoordinatorProtocol.swift
+//  skrudzh
+//
+//  Created by Alexander Petropavlovsky on 28/11/2018.
+//  Copyright © 2018 rubikon. All rights reserved.
+//
+
+import Foundation
+import PromiseKit
+
+protocol AccountCoordinatorProtocol {
+    func authenticate(email: String, password: String) -> Promise<Session>
+    func createAndAuthenticateUser(with userForm: UserCreationForm) -> Promise<Session>
+    func update(user: User) -> Promise<Void>
+    func changePassword(with changePasswordForm: ChangePasswordForm) -> Promise<Void>
+    func resetPassword(with resetPasswordForm: ResetPasswordForm) -> Promise<Void>
+    func createConfirmationCode(email: String) -> Promise<Void>
+    func loadCurrentUser() -> Promise<User>
+    func logout() -> Promise<Void>
+}
+
