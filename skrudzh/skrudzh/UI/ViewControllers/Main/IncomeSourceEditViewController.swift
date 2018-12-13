89aa12838dd89ef71b834afae5cd7cca1188d351
Alex Petropavlovsky <petalvlad@gmail.com>
2018-12-13 23:32:16 +0700

    Update income source edit view controller according to view model logic

--- skrudzh/skrudzh/UI/ViewControllers/Main/IncomeSourceEditViewController.swift
+++ skrudzh/skrudzh/UI/ViewControllers/Main/IncomeSourceEditViewController.swift
@@ -8,17 +8,32 @@
 
 import UIKit
 import StaticDataTableViewController
+import PromiseKit
 
-class IncomeSourceEditViewController : StaticDataTableViewController, NavigationBarColorable {
-    var navigationBarTintColor: UIColor? = UIColor.mainNavBarColor
+protocol IncomeSourceEditViewControllerDelegate {
+    func didCreate()
+}
+
+protocol IncomeSourceEditInputProtocol {
+    func set(incomeSource: IncomeSource)
+}
+
+class IncomeSourceEditViewController : StaticDataTableViewController, UIMessagePresenterManagerDependantProtocol, NavigationBarColorable {
     
     @IBOutlet weak var incomeSourceNameTextField: UITextField!
     @IBOutlet weak var activityIndicatorCell: UITableViewCell!
     @IBOutlet weak var saveButton: UIBarButtonItem!
+
+    var navigationBarTintColor: UIColor? = UIColor.mainNavBarColor
+
+    var viewModel: IncomeSourceEditViewModel!
+    var messagePresenterManager: UIMessagePresenterManagerProtocol!
+    var delegate: IncomeSourceEditViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        updateUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -28,15 +43,101 @@ class IncomeSourceEditViewController : StaticDataTableViewController, Navigation
     }
     
     @IBAction func didTapSaveButton(_ sender: Any) {
-        close()
+        save()
     }
     
     @IBAction func didTapCancelButton(_ sender: Any) {
         close()
     }
     
+    private func save() {
+        view.endEditing(true)
+        setActivityIndicator(hidden: false)
+        saveButton.isEnabled = false
+        
+        firstly {
+            viewModel.saveIncomeSource(with: self.incomeSourceNameTextField.text?.trimmed)
+        }.done {
+            if self.viewModel.isNew {
+                self.delegate?.didCreate()
+            }            
+            self.close()
+        }.catch { error in
+            switch error {
+            case IncomeSourceCreationError.validation(let validationResults):
+                self.showCreateionValidationResults(validationResults)
+            case IncomeSourceUpdatingError.validation(let validationResults):
+                self.showUpdatingValidationResults(validationResults)
+            case APIRequestError.unprocessedEntity(let errors):
+                self.show(errors: errors)
+            default:
+                self.messagePresenterManager.show(navBarMessage: "Ошибка при сохранении источника доходов",
+                                                  theme: .error)
+            }
+        }.finally {
+            self.setActivityIndicator(hidden: true)
+            self.saveButton.isEnabled = true
+        }
+    }
+    
     private func close() {
         navigationController?.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension IncomeSourceEditViewController {
+    private func show(errors: [String: String]) {
+        for (_, validationMessage) in errors {
+            messagePresenterManager.show(validationMessage: validationMessage)
+        }
+    }
+    
+    private func showCreateionValidationResults(_ validationResults: [IncomeSourceCreationForm.CodingKeys: [ValidationErrorReason]]) {
+        
+        for key in validationResults.keys.sorted(by: { $0.rawValue < $1.rawValue }) {
+            for reason in validationResults[key] ?? [] {
+                let message = creationValidationMessageFor(key: key, reason: reason)
+                messagePresenterManager.show(validationMessage: message)
+            }
+        }
+    }
+    
+    private func creationValidationMessageFor(key: IncomeSourceCreationForm.CodingKeys, reason: ValidationErrorReason) -> String {
+        switch (key, reason) {
+        case (.name, .required):
+            return "Укажите название"
+        case (_, _):
+            return "Ошибка ввода"
+        }
+    }
+    
+    private func showUpdatingValidationResults(_ validationResults: [IncomeSourceUpdatingForm.CodingKeys: [ValidationErrorReason]]) {
+        
+        for key in validationResults.keys.sorted(by: { $0.rawValue < $1.rawValue }) {
+            for reason in validationResults[key] ?? [] {
+                let message = updatingValidationMessageFor(key: key, reason: reason)
+                messagePresenterManager.show(validationMessage: message)
+            }
+        }
+    }
+    
+    private func updatingValidationMessageFor(key: IncomeSourceUpdatingForm.CodingKeys, reason: ValidationErrorReason) -> String {
+        switch (key, reason) {
+        case (.name, .required):
+            return "Укажите название"
+        case (_, _):
+            return "Ошибка ввода"
+        }
+    }
+}
+
+extension IncomeSourceEditViewController : IncomeSourceEditInputProtocol {
+    func set(incomeSource: IncomeSource) {
+        viewModel.set(incomeSource: incomeSource)
+    }
+    
+    private func updateUI() {
+        incomeSourceNameTextField.text = viewModel.name
     }
 }
 
