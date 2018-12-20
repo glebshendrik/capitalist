87a9b1e338ba687a264b524ef3be766d527ca968
Alex Petropavlovsky <petalvlad@gmail.com>
2018-12-20 19:51:14 +0700

    Implement launch screen

--- skrudzh/skrudzh/UI/ViewControllers/Join/LandingViewController.swift
+++ skrudzh/skrudzh/UI/ViewControllers/Join/LandingViewController.swift
@@ -11,6 +11,14 @@ import UIKit
 class LandingViewController : UIViewController {
     
     @IBOutlet weak var loadingMessageLabel: UILabel!
+    @IBOutlet weak var loaderImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loaderImageView.animationImages = [Int](1...15).compactMap { UIImage(named: "coin-loader-\($0)") }
+        loaderImageView.animationDuration = 1
+        loaderImageView.startAnimating()
+    }
     
     func update(loadingMessage: String) {
         loadingMessageLabel.text = loadingMessage
