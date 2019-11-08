14e60fed88f262c5d9337c937eb2a65d8a329d49
Alex Petropavlovsky <petalvlad@gmail.com>
2019-11-08 19:34:22 +0700

    Layout entity info view controller controls

---
+++ App/UI/ViewControllers/Common/EntityInfoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EntityInfoViewController.swift
+//  Three Baskets
+//
+//  Created by Alexander Petropavlovsky on 08/11/2019.
+//  Copyright © 2019 Real Tranzit. All rights reserved.
+//
+
+import Foundation
