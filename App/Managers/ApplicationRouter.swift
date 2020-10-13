602085f4393ccf80def916627be8ddc36d57e854
Alex Petropavlovsky <petalvlad@gmail.com>
2020-10-13 17:49:35 +0700

    Update SwiftyBeaver

--- App/Managers/ApplicationRouter.swift
+++ App/Managers/ApplicationRouter.swift
@@ -386,6 +386,8 @@ extension ApplicationRouter {
     }
     
     func showMainViewController() {
+        SwiftyBeaver.error(APIRequestError.methodNotAllowed)
+        SwiftyBeaver.info("Test info message")
         show(.MainViewController) { [weak self] in
             guard let self = self else { return }
             self.showPasscodeScreen()
